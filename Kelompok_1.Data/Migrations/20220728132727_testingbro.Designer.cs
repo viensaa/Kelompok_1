d3ae0f9ca7ea8e26a65ae34c292203e0834ca289
rizal <rizal@DESKTOP-23RBQ8Q>
2022-07-28 20:28:31 +0700

    testing db brooo

--- Kelompok_1.Data/Migrations/20220728132727_testingbro.Designer.cs
+++ Kelompok_1.Data/Migrations/20220728132727_testingbro.Designer.cs
@@ -4,6 +4,7 @@ using Kelompok_1.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Kelompok_1.Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20220728132727_testingbro")]
+    partial class testingbro
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Kelompok_1.Data/Migrations/20220728132727_testingbro.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kelompok_1.Data.Migrations
+{
+    public partial class testingbro : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
