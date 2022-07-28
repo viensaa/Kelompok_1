3db9c1275bab7866454fbaf3ef9cd7853da45014
rizal <rizal@DESKTOP-23RBQ8Q>
2022-07-28 17:44:08 +0700

    perbaikan

---
+++ Kelompok_1.Data/DAL/TransaksiDAL.cs
@@ -0,0 +1,44 @@
+﻿using Kelompok_1.Data.Interface;
+using Kelompok_1.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kelompok_1.Data.DAL
+{
+    public class TransaksiDAL : ITransaksi
+    {
+        public Task<Transaksi> AddExistingCartToTransaki(Transaksi obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Transaksi>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Transaksi> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Transaksi> Insert(Transaksi obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Transaksi> Update(Transaksi obj)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
